365ba4dd63f52ab5ce990a2964d952c4e17fb6b4
anna <lucille.gepecro@gmail.com>
2013-06-11 23:44:32 +0200

    Mise en forme repertoires + bean Ville

---
+++ src/betbikegame/beans/Ville.java
@@ -0,0 +1,79 @@
+package betbikegame.beans;
+
+import java.util.List;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+/**
+ * 
+ * @author anna
+ *
+ */
+@PersistenceCapable
+public class Ville {
+
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+	
+	@Persistent
+	private String name;
+	
+	@Persistent
+	private List<String> stations;
+	
+	@Persistent
+	private Integer available_bike_stands;
+	
+	@Persistent
+	private Float kilometers;
+
+	/*
+	 * GETTERS
+	 */
+	public Key getKey() {
+		return key;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public List<String> getStations() {
+		return stations;
+	}
+	
+	public Integer getAvailable_bike_stands() {
+		return available_bike_stands;
+	}
+	
+	public Float getKilometers() {
+		return kilometers;
+	}
+	
+	/*
+	 * SETTERS
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setStations(List<String> stations) {
+		this.stations = stations;
+	}
+
+	public void setAvailable_bike_stands(Integer available_bike_stands) {
+		this.available_bike_stands = available_bike_stands;
+	}
+
+	public void setKilometers(Float kilometers) {
+		this.kilometers = kilometers;
+	}
+	
+	
+	
+}
---
+++ src/betbikegame/servlets/LivreOrServlet.java
@@ -0,0 +1,37 @@
+package betbikegame.servlets;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.jdo.PersistenceManager;
+import javax.servlet.http.*;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import betbikegame.actions.PMF;
+import betbikegame.beans.Greeting;
+
+public class LivreOrServlet extends HttpServlet {
+    private static final Logger log = Logger.getLogger(LivreOrServlet.class.getName());
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+                throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+        String content = req.getParameter("content");
+        Date date = new Date();
+        Greeting greeting = new Greeting(user, content, date);
+
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        try {
+            pm.makePersistent(greeting);
+        } finally {
+            pm.close();
+        }
+
+        resp.sendRedirect("/thebetbikegame.jsp");
+    }
+}
